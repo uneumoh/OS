40317a1319363acb16ba8fff890e96528113d2eb
Une Umoh <une.umoh@pau.edu.ng>
2024-01-22 09:31:03 +0100

    jan

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+ loop {}
+}
+static HELLO: &[u8] = b"Hello World! This is
+just a quick illustration";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+ let framebuffer = 0xb8000 as *mut u8;
+ /*unsafe {
+ framebuffer.offset(1).write_volatile(0x30);
+ }*/
+ for (i, &byte) in HELLO.iter().enumerate() {
+ unsafe {
+ *framebuffer.offset(i as isize * 2) = byte;
+ *framebuffer.offset(i as isize * 2 + 1) = 0xb;
+ }
+ }
+ loop {}
+}
