c9a228ab38e191d258f7631fd3b9fa12eb3b78a4
Une Umoh <une.umoh@pau.edu.ng>
2024-01-24 18:28:28 +0100

    CA2

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 #![no_std]
 #![no_main]
+
+use x86_64::instructions::hlt;
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
- loop {}
+    loop {
+        hlt();
+    }
 }
 static HELLO: &[u8] = b"Hello World! This is
 just a quick illustration";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
- let framebuffer = 0xb8000 as *mut u8;
- /*unsafe {
- framebuffer.offset(1).write_volatile(0x30);
- }*/
- for (i, &byte) in HELLO.iter().enumerate() {
- unsafe {
- *framebuffer.offset(i as isize * 2) = byte;
- *framebuffer.offset(i as isize * 2 + 1) = 0xb;
- }
- }
- loop {}
+    let framebuffer = 0xb8000 as *mut u8;
+    /*unsafe {
+    framebuffer.offset(1).write_volatile(0x30);
+    }*/
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *framebuffer.offset(i as isize * 2) = byte;
+            *framebuffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {
+        hlt();
+    }
 }
